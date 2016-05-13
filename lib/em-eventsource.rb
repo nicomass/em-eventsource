84200ba68ae8e8a17f08ad3ffdef5de00bb7e8bf
nicomasscode <github@nicomass.com>
2016-05-13 19:55:46 -0400

    follow redirects

--- lib/em-eventsource.rb
+++ lib/em-eventsource.rb
@@ -186,7 +186,8 @@ module EventMachine
       headers = @headers.merge({'Cache-Control' => 'no-cache', 'Accept' => 'text/event-stream'})
       headers.merge!({'Last-Event-Id' => @last_event_id }) if not @last_event_id.nil?
       [conn, conn.get({ :query => @query,
-                        :head  => headers})]
+                        :head  => headers,
+			:redirects => 1})]
     end
   end
 end
